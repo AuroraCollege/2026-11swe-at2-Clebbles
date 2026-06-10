b171a4e3cfed638f6ce7795cc4ca895bba066e81
Caleb Loder <Caleb>
2026-06-10 13:30:16 +0000

    Finished developing the program, just ran into problem where port 5000 is eternally in use by another program, can (hopefully) be fixed

--- app.py
+++ app.py
@@ -1,8 +1,61 @@
 from flask import Flask, render_template, request
+import random
 from wumpus import HuntTheWumpus
 wumpus_game = HuntTheWumpus()
 
 app = Flask(__name__)
+
+MISSPELLED = 1
+CORRECT = 0
+
+with open("words.txt", "r", encoding="utf-8") as file:
+    words = [line.split() for line in file if line.strip()]
+
+spelling_list = [0] * 10
+misspelled_words = [0] * 10
+correct_words = [0] * 10
+results = [0] * 10
+#guesses = [0] * 10
+@app.route("/spelling_tester", methods=['POST', 'GET'])
+def spelling_tester():
+
+    for word_num in range (10):
+        num_used_already = True
+        while num_used_already:
+            num_used_already = False
+            random_word_num = random.randint(0,len(words)-1)
+            for search_num in reversed(range(word_num)):
+                if random_word_num == spelling_list[search_num]:
+                    num_used_already = True
+        spelling_list[word_num] = random_word_num
+
+    for word_num in range(10):
+        misspelled_words[word_num] = words[spelling_list[word_num]][MISSPELLED]
+        correct_words[word_num] = words[spelling_list[word_num]][CORRECT]
+
+    return render_template("spelling_tester.html" , misspelled_words = misspelled_words)
+
+@app.route("/submit", methods = ["POST"])
+def submit():
+    score = 0
+    guesses = request.form.getlist("guesses[]")
+    for guess_pointer in range(10):
+        if correct_words[guess_pointer] == guesses[guess_pointer]:
+            score += 1
+            results[guess_pointer] = "Correct!"
+        else:
+            results[guess_pointer] = "Incorrect!"
+
+    return render_template(
+        "results.html", 
+        misspelled_words = misspelled_words ,
+        guesses = guesses ,
+        correct_words = correct_words ,
+        results = results ,
+        score = score
+    )
+
+
 
 @app.route('/')
 def index():
@@ -16,13 +69,7 @@ def wumpus():
         message = wumpus_game.new_game()
     return render_template('wumpus.html', message=message, game=wumpus_game)
 
-@app.route('/spelling_tester', methods=['POST', 'GET'])
-def spelling_tester():
-    if request.method == 'POST':
-        message = spelling_tester.play(request.form)
-    else:
-        message = spelling_tester.new_game()
-    return render_template('spelling_tester.html', message=message, game=spelling_tester)
 
 if __name__ == "__main__":
     app.run()
+
--- spelling_tester.py
+++ spelling_tester.py
@@ -1,26 +1,3 @@
-import random
-
-def new_game(self):
-    self.player = Player()
-    self.wumpus = Wumpus(random.choice(list(self.rooms.keys())))
-    
-    self.game_on = True
-    return "Correct the 10 misspelled words"
+import random, render_template, request
 
 
-
-with open("words.txt", "r", encoding="utf-8") as file:
-    words = [line.split() for line in file if line.strip()]
-
-
-spelling_list = [0] * 10
-
-for word_num in range (10):
-    num_used_already = True
-    while num_used_already:
-        num_used_already = False
-        random_word_num = random.randint(0,len(words)-1)
-        for search_num in reversed(range(word_num)):
-            if random_word_num == spelling_list[search_num]:
-                num_used_already = True
-    spelling_list[word_num] = random_word_num
